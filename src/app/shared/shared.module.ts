a06696a8294462bd8f22616173b00a0744af87ad
duvan - salgado <dsalgado012@gmail.com>
2022-02-21 07:37:19 -0500

    creacion de footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,6 +6,7 @@ import { IonicModule } from '@ionic/angular';
 import { ButtonsActionsComponent } from './buttons-actions/buttons-actions.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { ComboboxComponent } from './combobox/combobox.component';
+import { FooterComponent } from './footer/footer.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -16,7 +17,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     SearchbarComponent,
     CalendarComponent,
     ButtonsActionsComponent,
-    ComboboxComponent
+    ComboboxComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -30,7 +32,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     SearchbarComponent,
     CalendarComponent,
     ButtonsActionsComponent,
-    ComboboxComponent
+    ComboboxComponent,
+    FooterComponent
   ]
 })
 export class SharedModule { }
