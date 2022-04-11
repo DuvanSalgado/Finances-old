d78b746fb5079de1fc5969e59c177ec3fe2005cb
duvan - salgado <dsalgado012@gmail.com>
2022-04-11 14:48:18 -0500

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDtXvVForJrQz5Y_Nek-aVNDqgZeDw7iGw',
+    authDomain: 'finance-9917e.firebaseapp.com',
+    projectId: 'finance-9917e',
+    storageBucket: 'finance-9917e.appspot.com',
+    messagingSenderId: '171624055655',
+    appId: '1:171624055655:web:74b9efdc0ea816fc13a5c2',
+    measurementId: 'G-BJXXKR6N9X'
+  }
 };
