9f1594a3f375a1af92379652e3c5ac401298816d
duvan - salgado <dsalgado012@gmail.com>
2022-04-22 17:19:09 -0500

    mejoras

--- src/app/home/credit/expenses/modal-add-expenses/modal-add-expenses.component.ts
+++ src/app/home/credit/expenses/modal-add-expenses/modal-add-expenses.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoadingService } from '@app/core/services/loading.service';
 import { ITEMSTYPE } from '@app/shared/combobox/model/data.combobox';
 import { Iicons, ITotal } from '@credit/model/credit.interface';
 import { FormExpensesCtrl } from '@credit/model/formCredit.enum';
@@ -7,12 +8,13 @@ import { mensages } from '@credit/model/menssage';
 import { Status } from '@credit/model/status.enum';
 import { CalculateService } from '@credit/service/calculate.service';
 import { ExpensesService } from '@credit/service/expenses.service';
-import { LoadingController, ModalController, ToastController } from '@ionic/angular';
+import { ModalController } from '@ionic/angular';
 import { format } from 'date-fns';
 
 @Component({
   selector: 'app-modal-add-expenses',
   templateUrl: './modal-add-expenses.component.html',
+  providers: [LoadingService]
 })
 export class ModalAddExpensesComponent implements OnInit {
 
@@ -23,7 +25,6 @@ export class ModalAddExpensesComponent implements OnInit {
   public formCtrl = FormExpensesCtrl;
   public itemsType = ITEMSTYPE;
 
-  private loadingModal: any;
   private todayDate = new Date();
 
   constructor(
@@ -31,27 +32,24 @@ export class ModalAddExpensesComponent implements OnInit {
     private formbuilder: FormBuilder,
     private calculateService: CalculateService,
     private expensesService: ExpensesService,
-    private toastController: ToastController,
-    private loadingController: LoadingController
+    private loadingService: LoadingService
   ) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
 
-  cancel(): void {
-    this.modalController.dismiss(false);
+  public async cancelModal(): Promise<void> {
+    await this.modalController.dismiss(false);
   }
 
   public async onSaveChange(evet: boolean): Promise<void> {
-    this.loading = true;
-    await this.presentLoading();
+    await this.loadingService.presentLoading();
     await this.calculate();
     await this.expensesService.create(this.formGroup.value);
-    await this.presentToast(mensages.successful);
-    this.loading = false;
-    await this.loadingModal.dismiss();
-    await this.modalController.dismiss();
+    await this.loadingService.presentToast(mensages.successful);
+    await this.loadingService.dismiss();
+    await this.cancelModal();
   }
 
   private initializeForm(): void {
@@ -92,16 +90,4 @@ export class ModalAddExpensesComponent implements OnInit {
     await this.calculateService.calculate(this.total);
   }
 
-  private async presentToast(mensaje: string): Promise<void> {
-    const toast = await this.toastController
-      .create({ message: mensaje, duration: 1050, position: 'top' });
-    toast.present();
-  }
-
-  private async presentLoading(): Promise<void> {
-    this.loadingModal = await this.loadingController.create({
-      message: 'Cargando...',
-    });
-    await this.loadingModal.present();
-  }
 }
