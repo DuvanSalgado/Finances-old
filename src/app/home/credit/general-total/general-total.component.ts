237b10e321cbdfc00b5c240afb5694d12eaaed11
duvan - salgado <dsalgado012@gmail.com>
2022-08-22 11:36:28 -0500

    error

--- src/app/home/credit/general-total/general-total.component.ts
+++ src/app/home/credit/general-total/general-total.component.ts
@@ -59,11 +59,13 @@ export class GeneralTotalComponent implements OnInit, OnDestroy {
     this.subscription = this.calculateService.getAll(month)
       .subscribe((data) => {
         if (data.length > 0) { this.total = data[0]; }
-        this.loadingService.dismiss();
       });
 
     this.subscription.add(this.calculateService.getAllCash()
-      .subscribe((data) => this.cashGeneral = data[0]));
+      .subscribe((data) => {
+        this.cashGeneral = data[0];
+        this.loadingService.dismiss();
+      }));
   }
 
 }
